078dbe3af329b3962a028bd8829947978e52db3d
Doğukan TANSUK <39658729+DogukanTansuk@users.noreply.github.com>
2022-05-26 16:27:42 +0300

    Cindoruk

--- App.test.tsx
+++ App.test.tsx
@@ -5,6 +5,7 @@ import App from "./App"
 
 describe("<App />", () => {
   it("has 1 child", () => {
+    // Cindoruk Was Here
     const tree: any = renderer.create(<App />).toJSON()
     expect(tree?.children?.length).toBe(1)
   })
